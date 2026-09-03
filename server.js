34278804e3dced2bfd37da550add38103e675db8
taachamisa-dev <taachamisa@gmail.com>
2026-09-03 19:11:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-require('./db/connection'); // applies schema on boot
+require('./connection'); // applies schema on boot
 
 const app = express();
 app.use(cors());
