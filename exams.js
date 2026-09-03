bb907b834c9850fd1262218b4f8f8fc08aa116ec
taachamisa-dev <taachamisa@gmail.com>
2026-09-03 19:37:30 +0200

    Update exams.js

--- exams.js
+++ exams.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
-const db = require('../db/connection');
+const db = require('./connection');
 const { authRequired, requireRole, subscriptionActive } = require('../middleware/auth');
 const { classAccessRequired } = require('../middleware/classAccess');
 const { percentageToUnits, computeClassPositions } = require('../utils/grading');
