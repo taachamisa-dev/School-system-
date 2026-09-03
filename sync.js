249a0668023f97726884a763d56c24276cb6c6c5
taachamisa-dev <taachamisa@gmail.com>
2026-09-03 20:37:15 +0200

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
 const db = require('./connection');
-const { authRequired, subscriptionActive } = require('../middleware/auth');
+const { authRequired, subscriptionActive } = require('./auth');
 
 const router = express.Router();
 router.use(authRequired, subscriptionActive);
